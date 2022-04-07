98ddf46365bb9817e849abc9e1b422a868963622
AZWAR <101930019+AZWAR480@users.noreply.github.com>
2022-04-07 16:00:47 +0700

    Update belajar struct elearnig.cpp
    ganti

--- belajar struct elearnig.cpp
+++ belajar struct elearnig.cpp
@@ -17,68 +17,25 @@ int main(){
     cout << "nim: " << mhs.nim << endl;
     cout << "nama: " << mhs.nama << endl;
     cout << "ipk: " << mhs.ipk << endl;
-}
-
-
-//#include <iostream>
-//using namespace std;
-//
-//int main(){
-//	//struct tungal (satu data)
-//	struct mahasiswa{
-//		int nim;
-//		char nama[20];
-//		float ipk;
-//	};
-//	
-//	mahasiswa mhs[2];
-//	for(int i = 0; i < 2; i++){
-//	    cin >> mhs[i].nim;
-//        cin >> mhs[i].nama;
-//        cin >> mhs[i].ipk;
-//    }
-//    
-//    for(int i =0; i < 2; i++){
-//    cout << "nim: " << mhs[i].nim << endl;
-//    cout << "nama: " << mhs[i].nama << endl;
-//    cout << "ipk: " << mhs[i].ipk << endl;
-//	}
-//    return 0;
-//}
     
-
-#include <iostream>
-#include <string.h>
-using namespace std;
-
-int main(){
-	//struct tungal (satu data)
-	struct buku{
-		int halaman;
-		char judul[10];
-	}b;
-	
-	b.halaman = 100;
-	strcpy(b.judul,"Belajar C");
-	
-	printf("%s=%d", b.judul,b.halaman);
-	return 0;
+    cout << "nim: " << mhs.nim << endl;
+    cout << "nama: " << mhs.nama << endl;
+    cout << "ipk: " << mhs.ipk << endl;
+    cout << "nim: " << mhs.nim << endl;
+    cout << "nama: " << mhs.nama << endl;
+    cout << "ipk: " << mhs.ipk << endl;
+    cout << "nim: " << mhs.nim << endl;
+    cout << "nama: " << mhs.nama << endl;
+    cout << "ipk: " << mhs.ipk << endl;
+    cout << "nim: " << mhs.nim << endl;
+    cout << "nama: " << mhs.nama << endl;
+    cout << "ipk: " << mhs.ipk << endl;cout << "nim: " << mhs.nim << endl;
+    cout << "nama: " << mhs.nama << endl;
+    cout << "ipk: " << mhs.ipk << endl;
+    
+    return 0;
 }
 
-//#include <iostream>
-//#include <string.h>
-//using namespace std;
-//
-//int main(){
-//	//struct tungal (satu data)
-//	struct mobil{
-//		int tahun;
-//		char warna[10];
-//	}mobil1,mobil2;
-//	
-//    mobil1.tahun=20100;
-//    mobil2 = mobil1;
-//	printf("mobil2=%d",mobil2.tahun);
-	getch();
-//	return 0;
-//}
+
+
+
